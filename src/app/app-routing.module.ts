d82d6f48fff524ee297140c9fbaad2b3be4307b2
Facundo <facundo@MacBook-Pro-de-Mac-2.local>
2023-04-08 00:55:24 -0300

    feat(contato):component and route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
+  {path:'contato', component:ContatoComponent},
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
